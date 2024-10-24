c2b8445238d77e163ff0c4f6cbcd1029d44f8401
TriBhaskar <panthribhaskar@gmail.com>
2024-10-24 21:21:33 +0530

    chore: redis connection config changes

--- src/main/java/org/triBhaskar/config/RedisConfig.java
+++ src/main/java/org/triBhaskar/config/RedisConfig.java
@@ -1,22 +1,46 @@
 package org.triBhaskar.config;
 
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import org.triBhaskar.scheduler.CoinApiScheduler;
 import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class RedisConfig {
+    private static final Logger log = LoggerFactory.getLogger(RedisConfig.class);
     @Value("${redis.url}")
     private String redisURL;
 
     @Bean
     public JedisPooled jedisPool() {
-        return new JedisPooled(redisURL);
+        int maxRetries = 5;
+        int retryCount = 0;
+        while (retryCount < maxRetries) {
+            try {
+                JedisPooled jedisPooled = new JedisPooled(redisURL);
+                String ping = jedisPooled.ping();// Test the connection
+               log.info("successfully connected to redis {}",ping);
+                return jedisPooled;
+            } catch (Exception e) {
+                retryCount++;
+                if (retryCount >= maxRetries) {
+                    throw new RuntimeException("Failed to connect to Redis after " + maxRetries + " attempts", e);
+                }
+                try {
+                    Thread.sleep(120000); // Wait for 2 minutes before retrying
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException("Thread interrupted during sleep", ie);
+                }
+            }
+        }
+        throw new RuntimeException("Failed to connect to Redis");
     }
+
     @Bean
     public Gson gson() {
         return new Gson();
