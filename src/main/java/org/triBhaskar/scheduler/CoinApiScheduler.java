93d35f7bd44e5f742d73779ffa2d0cd807f19157
TriBhaskar <panthribhaskar@gmail.com>
2024-10-24 21:21:33 +0530

    chore: redis connection config changes

--- src/main/java/org/triBhaskar/scheduler/CoinApiScheduler.java
+++ src/main/java/org/triBhaskar/scheduler/CoinApiScheduler.java
@@ -1,6 +1,8 @@
 package org.triBhaskar.scheduler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
